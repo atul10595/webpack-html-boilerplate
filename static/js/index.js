716dd31039820b63273ce717753f3e8750a66684
atul <atul_jain@bitbucket.org>
2018-08-31 13:37:07 +0530

    bootstrap 3 --> 4

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,14 @@
 import {debugLog, infoLog} from './custom/utils'
 
 require('jquery');
-require('normalize.css');
-require('bootstrap-loader');
-require('lodash/core');
+// require('normalize.css');
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// require('bootstrap-loader');
+
 
 // custom css
-import '../css/index.css'
+// import '../css/index.css'
 
 
 
